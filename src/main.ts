63b8c1ce867a829d58a7bca7ad30987ceff0ae00
涂迅嘉 <yangliu@yfpharmacy.com>
2026-05-19 17:20:37 +0800

    chore: 初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import { createRouter, createWebHashHistory } from 'vue-router';
 import { routes } from '@/router';
 import { setupRouterGuard } from '@/router/guard';
-import App from './App.vue';
-import '@/styles/reset.less';
 import '@/styles/global.less';
+import '@/styles/reset.less';
 import '@/styles/theme.less';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import App from './App.vue';
+
+const a = 1;
+console.log(a);
 
 const app = createApp(App);
 
